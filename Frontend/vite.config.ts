f5bf5fbe79ddeb1da44e5695218105ef51570775
riazmd3 <rockriaz5@gmail.com>
2025-08-02 21:04:48 +0530

    Updated Config file

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Navigation-bot/', // 👈 Your repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
